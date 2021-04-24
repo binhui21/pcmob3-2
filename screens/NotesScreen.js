6c163cd76bbb3ada6356cce605ad54231e404415
binhui21 <78149275+binhui21@users.noreply.github.com>
2021-04-24 18:29:34 +0800

    2nd commit

---
+++ screens/NotesScreen.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("notes.db");
+
+export default function NotesScreen({ route, navigation }) {
+  const [notes, setNotes] = useState([
+    { title: "Walk the cat", done: false, id: "0" },
+    { title: "Walk the dog", done: false, id: "1" },
+  ]);
+
+  function refreshNotes() {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM notes",
+        null,
+        (txObj, { rows: { _array } }) => setNotes(_array),
+        (txObj, error) => console.log("Error ", error)
+      );
+    });
+  }
+
+  useEffect(() => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          `CREATE TABLE IF NOT EXISTS
+          notes
+          (id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          done INT);`
+        );
+      },
+      null,
+      refreshNotes
+    );
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Entypo
+            name="new-message"
+            size={24}
+            color="black"
+            style={{ marginRight: 20 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  useEffect(() => {
+    if (route.params?.text) {
+      db.transaction(
+        (tx) => {
+          tx.executeSql("INSERT INTO notes (done, title) VALUES (0, ?)", [
+            route.params.text,
+          ]);
+        },
+        null,
+        refreshNotes
+      );
+    }
+  }, [route.params?.text]);
+
+  function addNote() {
+    navigation.navigate("Add Note");
+  }
+
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{ width: "100%" }}
+        data={notes}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
